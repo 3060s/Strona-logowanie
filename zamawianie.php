50db9a1366f245bf7a10473ffab675b5d0a789b3
3060s <78622374+3060s@users.noreply.github.com>
2023-11-09 21:33:26 +0100

    wyswietlanie ceny pizzy

--- zamawianie.php
+++ zamawianie.php
@@ -13,7 +13,6 @@
     $sql_query1 = mysqli_query($conn,$sql1);
 
     $id = mysqli_real_escape_string($conn,$_POST['pizza']); 
-
 ?>
 
 <!DOCTYPE html>
@@ -31,41 +30,32 @@
             <div class="txt_field">
                 <div class="dropdown">
                     <label>Wybierz pizze: </label>
-                    <select name="pizza">
-                        <?php 
-                    // use a while loop to fetch data 
-                    // from the $all_categories variable 
-                    // and individually display as an option
-                    while ($pizza = mysqli_fetch_array(
-                            $sql_query,MYSQLI_ASSOC)):; 
-                ?>
-                    <option value="<?php echo $pizza["id"];
-                    // The value we usually set is the primary key
-                    ?>">
-                        <?php echo $pizza["nazwa_pizzy"];
-                            // To show the category name to the user
-                        ?>
-                    </option>  
-                <?php 
-                endwhile; 
-                // While loop must be terminated
-                ?>  
-                </select>
+                        <select name="pizza">
+                            <?php 
+                        while ($pizza = mysqli_fetch_array(
+                                $sql_query,MYSQLI_ASSOC)):; 
+                    ?>
+                            <option value="<?php echo $pizza["id"];?>">
+                                <?php echo $pizza["nazwa_pizzy"], " ", $pizza["cena_pizzy"], "zł";?>
+                            </option>  
+                    <?php 
+                    endwhile; 
+                    ?>  
+                    </select>
                 </div>
 
 
-                <input type="number" name="ilosc" placeholder="Ilość" min="1"><br>
+                <input type="number" name="ilosc" placeholder="Ilość" min="1" max="15"><br>
                 <input type="text" name="imie" placeholder="Imie"><br>
                 <input type="text" name="nazwisko" placeholder="Nazwisko"><br>
                 <input type="tel" name="telefon" pattern="[0-9]{3}[0-9]{3}[0-9]{3}" maxlength="9" placeholder="Numer telefonu (xxx-xxx-xxx)"><br>
                 <input type="email" name="email" placeholder="Email"><br>
                 <input type="text" name="miasto" placeholder="Miasto"><br>
                 <input type="text" name="ulica" placeholder="Ulica"><br>
-                <input type="text" name="nr_domu" placeholder="Numer domu/mieszkania"><br>
-                
+                <input type="text" name="nr_domu" placeholder="Numer domu/mieszkania"><br>               
 
             </div>
-                <input type="submit" name="submit" value="Zamów" class="submit"><br><br><br>   
+                <input type="submit" name="submit" value="Zamów" class="submit" onclick="alertfn()"><br><br><br>   
         </form>
     </div>      
 </body>
@@ -75,8 +65,7 @@
 
     if(isset($_POST['submit'])){
 
-
-        $ilosc = $_POST['ilosc'];
+        $ilosc =$_POST['ilosc'];
         $imie = $_POST['imie'];
         $nazwisko = $_POST['nazwisko'];
         $telefon = $_POST['telefon'];
@@ -90,7 +79,13 @@
 
 
         if(empty($imie) || empty($nazwisko) || empty($telefon) || empty($email) || empty($miasto) || empty($ulica) || empty($nr_domu)) {
-            echo 'Niepoprawne dane logowania';
+            //echo "<script>";
+            //echo "alert('Złóż poprawnie zamówienie!');";
+            //echo "</script>";
+            echo '<script type="text/javascript">
+                    alert("Hello, World!");
+                  </script>';
+
             $errflag= true;
         }
 
@@ -110,4 +105,4 @@
     if ( window.history.replaceState ) {
         window.history.replaceState( null, null, window.location.href );
     }
-</script>    
+</script>
